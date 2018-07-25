e396959e1cd3802fb4f76e90cd44e4f4e8e80149
Scott Daniel <scottberry91@gmail.com>
2018-07-25 13:52:49 +0100

    Initial Commit

---
+++ src/hero.rs
@@ -0,0 +1,48 @@
+use diesel;
+use diesel::mysql::MysqlConnection;
+use diesel::prelude::*;
+use schema::heroes;
+
+#[table_name = "heroes"]
+#[derive(Serialize, Deserialize, Queryable, Insertable)]
+pub struct Hero {
+    pub id: Option<i32>,
+    pub name: String,
+    pub identity: String,
+    pub hometown: String,
+    pub age: i32,
+}
+
+impl Hero {
+    pub fn create(hero: Hero, connection: &MysqlConnection) -> Hero {
+        diesel::insert_into(hereos::table)
+            .values(&hero)
+            .execute(connection)
+            .expect("Error creating new hero");
+
+        heroes::table
+            .order(heroes::id.desc())
+            .first(connection)
+            .unwrap()
+    }
+
+    pub fn read(connection: &MysqlConnection) -> Vec<Hero> {
+        heroes::table
+            .order(heroes::id.asc())
+            .load::<Hero>(connection)
+            .unwrap()
+    }
+
+    pub fn update(id: i32, hero: Hero, connection: &MysqlConnection) -> bool {
+        diesel::update(heroes::table.find(id))
+            .set(&hero)
+            .execute(connection)
+            .is_ok()
+    }
+
+    pub fn delete(id: i32, connection: &MysqlConnection) -> bool {
+        diesel::delete(heroes::table.find(id))
+            .execute(connection)
+            .is_ok()
+    }
+}
